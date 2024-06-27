aa0ae963990e611d2138222890fc4ff38300ed43
vortexzjs <gordinhogamer81@gmail.com>
2024-06-26 23:44:15 -0300

    modified:   index.js 	modified:   requirements.txt

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
 const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
 
@@ -16,35 +13,21 @@ app.get('/download', async (req, res) => {
   // Execute o script Python
   exec(`python3 downloader.py ${videoURL}`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send('Error downloading video');
+        console.error(`Erro ao executar o comando: ${error.message}`);
+        return res.status(500).json({ error: 'Erro ao baixar o vídeo' });
+    }
+    if (stderr) {
+        console.error(`Erro no stderr: ${stderr}`);
+        return res.status(500).json({ error: 'Erro ao baixar o vídeo' });
     }
 
-    // Verifique se o vídeo foi baixado
-    const filePath = path.resolve(__dirname, 'downloaded_video.mp4');
-    if (fs.existsSync(filePath)) {
-      res.download(filePath, 'video.mp4', (err) => {
-        if (err) {
-          console.error(`download error: ${err}`);
-          res.status(500).send('Error sending file');
-        } else {
-          // Remova o arquivo após o download
-          fs.unlinkSync(filePath);
-        }
-      });
-    } else {
-      res.status(500).send('Video not found');
-    }
+    console.log(`Saída do comando: ${stdout}`);
+    res.json({ message: 'Vídeo baixado com sucesso' });
   });
-  
-//   res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-//   ytdl(videoURL, {
-//     format: 'mp4'
-//   }).pipe(res);
-
-});
+}
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor rodando em: ${PORT}`);
 });
